0c1d122f5c366f5b10abba8b49b10bdae59ead9f
KFC <kfclts@gmail.com>
2023-02-18 17:49:12 +0800

    Intial commit

---
+++ src/main/java/com/kfc/ocpdemo/OcpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.kfc.ocpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpdemoApplication.class, args);
+	}
+
+}
